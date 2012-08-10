4a2bda89cff2dfd33ab2f2cfa38485fcb9d00e17
coquin <denis.kokin@gmail.com>
2012-08-10 19:30:15 +0400

    Jade templating engine (default) replaced with handlebars.js (using https://github.com/donpark/hbs).

--- app.js
+++ app.js
@@ -6,14 +6,16 @@
 var express = require('express')
   , routes = require('./routes')
   , http = require('http')
-  , path = require('path');
+  , path = require('path')
+  , hbs = require('hbs');
 
 var app = express();
 
 app.configure(function(){
   app.set('port', process.env.PORT || 3000);
   app.set('views', __dirname + '/views');
-  app.set('view engine', 'jade');
+  app.engine('html', hbs.__express);
+  app.set('view engine', 'html');
   app.use(express.favicon());
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
