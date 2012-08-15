8d066aa5753a271be93a3ca448cdbf59fa353d77
coquin <denis.kokin@gmail.com>
2012-08-15 19:32:52 +0400

    Naive search engine crawler requests bypassing through PhantomJS implemented.

--- app.js
+++ app.js
@@ -4,6 +4,8 @@
  */
 
 var express = require('express')
+  , exec = require('child_process').exec
+  , useragent = require('express-useragent')
   , routes = require('./routes')
   , http = require('http')
   , path = require('path')
@@ -12,7 +14,7 @@ var express = require('express')
 var app = express();
 
 app.configure(function(){
-  app.set('port', process.env.PORT || 3000);
+  app.set('port', process.env.PORT || 8888);
   app.set('views', __dirname + '/views');
   app.engine('html', hbs.__express);
   app.set('view engine', 'html');
@@ -20,6 +22,7 @@ app.configure(function(){
   app.use(express.logger('dev'));
   app.use(express.bodyParser());
   app.use(express.methodOverride());
+  app.use(useragent.express());
   app.use(app.router);
   app.use(express.static(path.join(__dirname, 'public')));
 });
@@ -28,7 +31,22 @@ app.configure('development', function(){
   app.use(express.errorHandler());
 });
 
-app.get('/', routes.index);
+app.get('/', function(req, res) {
+  // Match only Googlebot search engine crawler
+  var seCraulersUA_Match = /Googlebot|\+http:\/\/www\.google\.com\/bot.html/gi;
+
+  if (seCraulersUA_Match.test(req.useragent.source)) {
+    console.log('>>> Search engine crawler detected! <<<');
+
+    // Asyncronously load requested page in PhantomJS headless browser,
+    // receive generated page source and send it to the crawler
+    exec('phantomjs proxy.js http://' + req.headers.host, function(error, stdout) {
+      res.send(stdout);
+    });
+  } else {
+    res.render('index');
+  }
+});
 
 http.createServer(app).listen(app.get('port'), function(){
   console.log("Express server listening on port " + app.get('port'));
