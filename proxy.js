c0a1dea4bd2573bb16a8e6906a0f6a1e47e2198b
coquin <denis.kokin@gmail.com>
2012-08-16 14:42:54 +0400

    Extra timeout added to give a page some time to render.

--- proxy.js
+++ proxy.js
@@ -4,12 +4,16 @@ var page = require('webpage').create(),
     url = phantom.args[0];
 
 page.onLoadFinished = function(status) {
-  // Save page screenshot
-  page.render('page_snapshot.png');
+  // Just give a page 3 seconds to render
+  setTimeout(function() {
+    // Save page screenshot
+    page.render('page_snapshot.png');
 
-  // Respond with the page content
-  console.log(page.content);
-  phantom.exit();
+    // Respond with the page content
+    console.log(page.content);
+    phantom.exit();
+  }, 3000);
 };
 
+page.viewportSize = { width: 800, height: 500 };
 page.open(url);
